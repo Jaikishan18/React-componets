b3ffd92f63372be028da3f0c3ec0ec831555e90f
Jaikishan18 <kishangv7@gmail.com>
2024-01-02 14:36:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useEffect, useMemo, useState } from "react";
 
-function App(){
-  return(
-    <div className='App'>
-    <Profile/>
+function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+
+  const themestyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+  const doubleNumber = useMemo(() => {
+    return multiplication(number);
+  }, [number]);
+  useEffect(() => {
+    console.log("number changed");
+  }, [number]);
+
+  return (
+    <div className="container">
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => {
+          setNumber(parseInt(e.target.value));
+        }}
+      ></input>
+
+      <button
+        onClick={() => {
+          setDark(!dark);
+        }}
+      >
+        toggle
+      </button>
+
+      <div className="double" style={themestyles}>
+        <p>This is the double container</p>
+        <h4>number:{doubleNumber}</h4>
+      </div>
     </div>
-  )
+  );
 }
- 
+
+function multiplication(num) {
+  return num * 2;
+}
 export default App;
-
-
-function Profile(){
-  return(
-    <div>
-      <h2>Profile Name</h2>
-    </div>
-  )
-}
